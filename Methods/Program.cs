c11cfc2ebf99bf50a9af12e6c85f8c62b1f9ea6b
AnnKemp <ann.kemp@scarlet.be>
2021-10-12 21:25:44 +0200

    with multiple parameters + return, so NOT void

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,15 +4,14 @@ namespace Methods
 {
     class Program
     {
-        /* The void keyword, used in the examples above, indicates that the method should not return a value. If you want the method to return a value, you can use a primitive data type (such as int or double) instead of void, and use the return keyword inside the method*/
-        static int MyMethod(int x)
+        static int MyMethod(int x, int y)
         {
-            return 5 + x;
+            return x + y;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(MyMethod(3));
+            Console.WriteLine(MyMethod(5, 3));
         }
 
     }
