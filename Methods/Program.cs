909d87337f929ba7571f7b2d0fc7d6e390499e87
AnnKemp <ann.kemp@scarlet.be>
2021-10-12 21:07:21 +0200

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
