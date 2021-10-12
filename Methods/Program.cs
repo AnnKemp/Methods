02d2e6b72a08322bf3cfa9f77b79e6788cd3266c
AnnKemp <ann.kemp@scarlet.be>
2021-10-12 21:34:54 +0200

    methods: overloading

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,15 +4,22 @@ namespace Methods
 {
     class Program
     {
-        //Named arguments are especially useful when you have multiple parameters with default values, and you only want to specify one of them when you call it:
-        static void MyMethod(string child1 = "Liam", string child2 = "Jenny", string child3 = "John")
+        static int PlusMethod(int x, int y)
         {
-            Console.WriteLine(child3);
+            return x + y;
+        }
+
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
         }
 
         static void Main(string[] args)
         {
-            MyMethod("child3");
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
         }
 
     }
